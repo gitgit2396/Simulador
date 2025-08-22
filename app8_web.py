bee82415fd4dad77fa65a1795d2dfd2920c4455f
gitgit2396 <juan.acosta.ecom@gmail.com>
2025-08-22 10:39:04 -0300

    Remove unused matplotlib import

--- app8_web.py
+++ app8_web.py
@@ -8,7 +8,6 @@ import pandas as pd
 import numpy as np
 import yaml
 import io
-import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
